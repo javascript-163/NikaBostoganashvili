1eff6addd1b08206912aeb9df2fbb298d2721fe6
Nika Bostoganashvili <151997372+theBosto@users.noreply.github.com>
2024-02-17 19:53:33 +0400

    updated

--- Lesson9Homework/scripts/task1.js
+++ Lesson9Homework/scripts/task1.js
@@ -1,47 +1,52 @@
 //task 1
 function readFile() {
-  return new Promise(() => {
-    let chooseFile = document.getElementById("chooseFile");
-    let file = chooseFile.files[0];
+  const chooseFile = document.getElementById("chooseFile");
+  let file = chooseFile.files[0];
+  if (file) {
+    const reader = new FileReader();
 
-    if (file) {
-      const reader = new FileReader();
+    reader.readAsText(file);
 
-      reader.readAsText(file);
-
-      reader.onload = function (event) {
-        document.getElementById("textInput").value = event.target.result;
-      };
-    }
-
-    let text = document.getElementById("textInput").value;
-
-    //total words
-    let splitted = text.split(" ");
-    totalWords.innerHTML = `Total words: ${splitted.length}`;
-
-    //total symbols
-    let symbols = [];
-    for (let i of text) {
-      symbols.push(i);
-    }
-    totalSymbols.innerHTML = `Total Symbols: ${symbols.length}`;
-
-    //longest word
-    let longest = "";
-    for (let i of splitted) {
-      if (i.length > longest.length) {
-        longest = i;
+    reader.onload = function (event) {
+      let text = document.getElementById("textInput");
+      text.value = event.target.result;
+      // total symbols
+      let symbols = [];
+      for (let i of text.value) {
+        symbols.push(i);
       }
-    }
-    longestWord.innerHTML = `Longest Word: ${longest}`;
+      totalSymbols.textContent = `Total Symbols: ${symbols.length}`;
 
-    //shortest word
-    let shortest = "";
-    splitted.sort(function (a, b) {
-      return a.length - b.length;
-    });
-    shortest = splitted[0];
-    shortestWord.innerHTML = `Shortest Word: ${shortest}`;
-  });
+      //total words
+      let splitted = text.value.split(" ");
+      totalWords.textContent = `Total words: ${splitted.length}`;
+
+      //longest word
+      let longest = "";
+      for (let i of splitted) {
+        if (i.length > longest.length) {
+          longest = i;
+        }
+      }
+      longestWord.textContent = `Longest Word: ${longest}`;
+
+      //shortest word
+      let shortest = "";
+      splitted.sort(function (a, b) {
+        return a.length - b.length;
+      });
+      shortest = splitted[0];
+      shortestWord.textContent = `Shortest Word: ${shortest}`;
+
+      //Average
+      let sum = 0;
+      let averageNum = 0;
+      for (let i = 0; i < splitted.length; i++) {
+        sum += splitted[i].length;
+      }
+      averageNum = sum / splitted.length;
+
+      average.textContent = `Average length of words: ${averageNum}`;
+    };
+  }
 }
